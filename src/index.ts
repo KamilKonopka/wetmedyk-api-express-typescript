dcbfd33a16b00fbc92e38dda7c84882484a8b5b0
kamilkonopka <city3studio@gmail.com>
2020-04-08 17:29:05 +0200

    Added config for different environments

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import cors from 'cors';
+import config from 'config';
 import { router as posts } from './routes/posts';
 import { router as employees } from './routes/employees';
 import { router as locations } from './routes/locations';
 import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 
@@ -18,6 +20,13 @@ app.use(bodyParser.json());
 app.use('/posts', posts);
 app.use('/employees', employees);
 app.use('/locations', locations);
+
+if (app.get('env') === 'development') {
+    app.use(morgan('tiny'));
+}
+
+// tslint:disable-next-line:no-console
+console.log(config.get('name'));
 
 app.get('/', (req, res) => {
     res.send('Test');
--- src/routes/locations.ts
+++ src/routes/locations.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { connectionConfig } from "../database/mysql-db";
 import mysql from 'mysql';
 import {prepareQuery} from "../database/queries";
+import * as _ from "lodash";
 
 export const router = express.Router();
 
@@ -9,10 +10,11 @@ router.get('/', (req, res) => {
     const sql = prepareQuery('locations');
     const connection = mysql.createConnection(connectionConfig);
     connection.connect();
-    connection.query(sql, (err, results, fields) => {
+    connection.query(sql, (err, results: { [key: string]: string | number }[], fields) => {
         if (err) {
             return new Error(err.message);
         } else {
+
             return results ? res.send(results) : res.status(404).send('Posts not found');
         }
     });
@@ -32,3 +34,12 @@ router.get('/:id', ((req, res) => {
     });
     connection.end();
 }));
+
+// export function mapFieldsIntoCamelCase (results: { [key: string]: string | number }[]): object[] {
+//     return results.map((el: { [key: string]: string | number }) => {
+//         return Object.entries(el).reduce((obj: {}, [key, value]) => {
+//             obj[key] = value;
+//             return obj;
+//         }, {});
+//     });
+// }
